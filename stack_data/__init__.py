adc83587d59c170068750ca670a3107a6695ea78
Alex Hall <alex.mojaki@gmail.com>
2019-12-08 18:39:01 +0200

    Export RepeatedFrames from __init__

--- stack_data/__init__.py
+++ stack_data/__init__.py
@@ -1,3 +1,3 @@
 __version__ = '0.0.1'
 
-from .core import Source, FrameInfo, markers_from_ranges, Options, LINE_GAP, Line, Variable, Range
+from .core import Source, FrameInfo, markers_from_ranges, Options, LINE_GAP, Line, Variable, Range, RepeatedFrames
