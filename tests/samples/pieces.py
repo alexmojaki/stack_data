5d7de7c963b042870844f0869f567e186a03e8f7
Alex Hall <alex.mojaki@gmail.com>
2019-12-08 17:41:46 +0200

    Test splitting file into pieces. FIx test_skipping_frames

---
+++ tests/samples/pieces.py
@@ -0,0 +1,54 @@
+import math
+
+
+def foo(x=1, y=2):
+    z = 0
+    for i in range(5):
+        z += i * x * math.sin(y)
+        z += math.copysign(
+            -1,
+            2,
+        )
+
+    for i in range(
+            0,
+            6
+    ):
+        try:
+            str(i)
+        except:
+            pass
+
+        try:
+            int(i)
+        except (ValueError,
+                TypeError):
+            pass
+        finally:
+            str("""
+            foo
+            """)
+
+
+def foo2(
+        x=1,
+        y=2,
+):
+    while 9:
+        while (
+                9 + 9
+        ):
+            if 1:
+                pass
+            elif 2:
+                pass
+            elif (
+                    3 + 3
+            ):
+                pass
+            else:
+                pass
+
+
+class Foo(object):
+    pass
--- tests/test_core.py
+++ tests/test_core.py
@@ -1,5 +1,6 @@
 import inspect
 import re
+from pathlib import Path
 
 from stack_data import Options, Line, LINE_GAP
 from stack_data import Source, FrameInfo
@@ -49,6 +50,65 @@ def test_lines_with_gaps():
     ]
 
 
+def test_pieces():
+    filename = Path(__file__).parent / "samples/pieces.py"
+    source = Source.for_filename(str(filename))
+    pieces = [
+        [
+            source.lines[i - 1]
+            for i in range(start, end)
+        ]
+        for start, end in source.pieces
+    ]
+    assert pieces == [
+        ['import math'],
+        ['def foo(x=1, y=2):'],
+        ['    z = 0'],
+        ['    for i in range(5):'],
+        ['        z += i * x * math.sin(y)'],
+        ['        z += math.copysign(',
+         '            -1,',
+         '            2,',
+         '        )'],
+        ['    for i in range(',
+         '            0,',
+         '            6',
+         '    ):'],
+        ['        try:'],
+        ['            str(i)'],
+        ['        except:'],
+        ['            pass'],
+        ['        try:'],
+        ['            int(i)'],
+        ['        except (ValueError,',
+         '                TypeError):'],
+        ['            pass'],
+        ['        finally:'],
+        ['            str("""',
+         '            foo',
+         '            """)'],
+        ['def foo2(',
+         '        x=1,',
+         '        y=2,', '):'],
+        ['    while 9:'],
+        ['        while (',
+         '                9 + 9',
+         '        ):'],
+        ['            if 1:'],
+        ['                pass'],
+        ['            elif 2:'],
+        ['                pass'],
+        ['            elif (',
+         '                    3 + 3',
+         '            ):'],
+        ['                pass'],
+        ['            else:'],
+        ['                pass'],
+        ['class Foo(object):'],
+        ['    pass'],
+    ]
+
+
 def test_skipping_frames():
     def factorial(n):
         if n <= 1:
@@ -85,8 +145,9 @@ def test_skipping_frames():
             simple_frame(foo),
             simple_frame(factorial),
             simple_frame(foo),
-            ("test_skipping_frames.<locals>.factorial at line 56 (16 times), " +
-             "test_skipping_frames.<locals>.foo at line 59 (16 times)"),
+            ("test_skipping_frames.<locals>.factorial at line {factorial} (16 times), "
+             "test_skipping_frames.<locals>.foo at line {foo} (16 times)"
+             ).format(**linenos),
             simple_frame(factorial),
             simple_frame(foo),
             (factorial.__code__, linenos["exception"]),
