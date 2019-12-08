3672ac02acad3108874a8a203b6547933b99c67c
Alex Hall <alex.mojaki@gmail.com>
2019-12-08 16:20:14 +0200

    Make truncate include more items at the beginning than at the end

--- stack_data/utils.py
+++ stack_data/utils.py
@@ -5,8 +5,8 @@ from collections import OrderedDict, Counter
 
 def truncate(seq, max_length, middle):
     if len(seq) > max_length:
-        left = (max_length - len(middle)) // 2
-        right = max_length - len(middle) - left
+        right = (max_length - len(middle)) // 2
+        left = max_length - len(middle) - right
         seq = seq[:left] + middle + seq[-right:]
     return seq
 
