0303449f41eab7e4064e301bb9eb0d046ba45a84
Alex Hall <alex.mojaki@gmail.com>
2020-04-07 20:14:18 +0200

    Fix check for version argument

--- make_release.sh
+++ make_release.sh
@@ -1,20 +1,21 @@
 #!/usr/bin/env bash
 set -eux;
 
-if [${1+x}]; then
+if [ -z "${1+x}" ]; then
+    set +x
+    echo Provide a version argument
+    echo "${0} <major>.<minor>.<patch>";
+    exit 1;
+else
     if [[ ${1} =~ ^v?([0-9]+)(\.[0-9]+)?(\.[0-9]+)?$ ]]; then
         :;
     else
         echo "Not a valid release tag.";
         exit 1;
     fi;
-else
-    echo "${0} <major>.<minor>.<patch>";
-    exit 1;
 fi;
 
 export TAG="v${1}";
-echo "$TAG"
 git tag "${TAG}";
 git push origin master "${TAG}";
 rm -rf ./build ./dist;
