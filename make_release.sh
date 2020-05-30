3eb0c992d476c35bcdd930cc622078f21bed86a8
Alex Hall <alex.mojaki@gmail.com>
2020-05-30 16:41:06 +0200

    Remove semicolons from make_release

--- make_release.sh
+++ make_release.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
-set -eux;
+set -eux
 
 if [ -z "${1+x}" ]; then
     set +x
     echo Provide a version argument
-    echo "${0} <major>.<minor>.<patch>";
-    exit 1;
+    echo "${0} <major>.<minor>.<patch>"
+    exit 1
 else
     if [[ ${1} =~ ^v?([0-9]+)(\.[0-9]+)?(\.[0-9]+)?$ ]]; then
-        :;
+        :
     else
-        echo "Not a valid release tag.";
-        exit 1;
-    fi;
-fi;
+        echo "Not a valid release tag."
+        exit 1
+    fi
+fi
 
-export TAG="v${1}";
-git tag "${TAG}";
-git push origin master "${TAG}";
-rm -rf ./build ./dist;
-python3 -m pep517.build -b .;
-twine upload ./dist/*.whl;
+export TAG="v${1}"
+git tag "${TAG}"
+git push origin master "${TAG}"
+rm -rf ./build ./dist
+python3 -m pep517.build -b .
+twine upload ./dist/*.whl
