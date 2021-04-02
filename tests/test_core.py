123d8de59955024cd5070348e81825997d2333db
Alex Hall <alex.mojaki@gmail.com>
2021-04-02 16:38:19 +0200

    Update tests for new pure_eval

--- tests/test_core.py
+++ tests/test_core.py
@@ -248,6 +248,20 @@ def test_variables():
             value=options,
         ),
         Variable(
+            name='str(x)',
+            nodes=(
+                body[3].value,
+            ),
+            value='{982347298304}',
+        ),
+        Variable(
+            name='str(y)',
+            nodes=(
+                body[1].value,
+            ),
+            value='123986',
+        ),
+        Variable(
             name='x',
             nodes=(
                 body[2].targets[0],
@@ -275,7 +289,7 @@ def test_variables():
 
     assert (
             sorted(frame_info.variables_in_lines) ==
-            variables[:3]
+            [*variables[:3], variables[4]]
     )
 
 
